c100298dc8a0c2ae145b958f2405ca0a872c4f0e
Meisuleen Kased <meisuleen@gmail.com>
2018-11-01 02:54:03 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,14 @@
 require 'pry'
 def unsafe?(speed) #return true if the speed is unsafe and false if it is safe.
-	speed > 60 ? true : false
-	speed < 40 ? true : false
+	if speed > 60
+		return true 
+	elsif speed < 40 
+		return true
+	else 
+		return false
+	end	
+	# speed > 60 ? true : false
+	# speed < 40 ? true : false
 end
 
 
